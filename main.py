b6fc3335a50649e3f295c936315e07c0911249b7
Kimberley Werezak <122053493+kimwerezak@users.noreply.github.com>
2024-06-06 14:44:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #Your Kitronik robot will move in a square by moving forward at speed 20 for a 
 #half second, and repeating this process 3 more times.  
 #Movement of the robot on different surfaces my provide different results.
-for i in range(4):
-    Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 20)
-    basic.pause(500)
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.LEFT, 27)
-    basic.pause(500)
-Kitronik_Move_Motor.stop()
+
+def drive_square(speed_forward, speed_turn):
+    for i in range(4):
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, speed_forward)
+        basic.pause(500)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.LEFT, speed_turn)
+        basic.pause(500)
+    Kitronik_Move_Motor.stop()
+
+drive_square(20,27)
