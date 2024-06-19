4530fc8f3d266f721a78afed0ea1878f355cad5c
Kimberley Werezak <122053493+kimwerezak@users.noreply.github.com>
2024-06-19 13:54:55 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 // Your Kitronik robot will move in a square by moving forward at speed 20 for a 
 // half second, and repeating this process 3 more times.  
 // Movement of the robot on different surfaces my provide different results.
-for (let i = 0; i < 4; i++) {
-    Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
-    basic.pause(500)
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 27)
-    basic.pause(500)
+function drive_square(speed_forward: number, speed_turn: number) {
+    for (let i = 0; i < 4; i++) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed_forward)
+        basic.pause(500)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, speed_turn)
+        basic.pause(500)
+    }
+    Kitronik_Move_Motor.stop()
 }
-Kitronik_Move_Motor.stop()
+
