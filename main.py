e41877105d7d704a5ed555cf9e6cb54a94e326e4
Kimberley Werezak <122053493+kimwerezak@users.noreply.github.com>
2025-04-15 13:33:53 -0600

    Recreated to be a challenged for students.

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-#Your Kitronik robot will move in a square by moving forward at speed 20 for a 
-#half second, and repeating this process 3 more times.  
-#Movement of the robot on different surfaces my provide different results.
+#Challenge: simplify and complete the following code.  The robot must move in a square.
 
-def drive_square(speed_forward, speed_turn):
-    for i in range(4):
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, speed_forward)
-        basic.pause(500)
-        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.LEFT, speed_turn)
-        basic.pause(500)
-    Kitronik_Move_Motor.stop()
+
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 20)
+basic.pause(1000)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.RIGHT, 20)
+basic.pause(1200)
+
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 20)
+basic.pause(1000)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.RIGHT, 20)
+basic.pause(1200)
+
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 20)
+basic.pause(1000)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.RIGHT, 20)
+basic.pause(1200)
+
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 20)
+basic.pause(1000)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.RIGHT, 20)
+basic.pause(1200)
+
+Kitronik_Move_Motor.stop()
--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
-// Your Kitronik robot will move in a square by moving forward at speed 20 for a 
-// half second, and repeating this process 3 more times.  
-// Movement of the robot on different surfaces my provide different results.
-function drive_square(speed_forward: number, speed_turn: number) {
-    for (let i = 0; i < 4; i++) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed_forward)
-        basic.pause(500)
-        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, speed_turn)
-        basic.pause(500)
-    }
-    Kitronik_Move_Motor.stop()
-}
-
+// Challenge: simplify and complete the following code.  The robot must move in a square.
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
+basic.pause(1000)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Right, 20)
+basic.pause(1200)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
+basic.pause(1000)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Right, 20)
+basic.pause(1200)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
+basic.pause(1000)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Right, 20)
+basic.pause(1200)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
+basic.pause(1000)
+Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Right, 20)
+basic.pause(1200)
+Kitronik_Move_Motor.stop()
